afdc059e9dd22b6f3a9c0fab5764a24a6d943595
George Keloglou <geokeloglou@gmail.com>
2019-08-03 21:52:51 +0300

    Spinner & form error handling

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -39,13 +39,10 @@ export const auth = (email, password, isSignup) => {
     }
     axios.post(url, authData)
       .then(response => {
-        console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
       })
       .catch(err => {
-        console.log(err);
-        console.log(KEY);
-        dispatch(authFail(err));
+        dispatch(authFail(err.response.data.error));
       });
   };
 };
