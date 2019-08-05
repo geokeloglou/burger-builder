b4eea83f92b7ec18b941578c3c09577e251e1d3e
George Keloglou <geokeloglou@gmail.com>
2019-08-05 20:02:57 +0300

    pass userId to filter by, add user properties dispatch action to fetch orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrders(this.props.idToken);
+    this.props.onFetchOrders(this.props.idToken, this.props.userId);
   }
 
   render() {
@@ -33,13 +33,14 @@ const mapStateToprops = state => {
   return {
     orders: state.order.orders,
     loading: state.order.loading,
-    idToken: state.auth.idToken
-  }
+    idToken: state.auth.idToken,
+    userId: state.auth.userId
+  };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
+    onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
   };
 };
 
