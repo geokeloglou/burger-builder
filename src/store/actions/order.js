d76f50c22f5fa96f320707d182a787c7ef1a8279
George Keloglou <geokeloglou@gmail.com>
2019-08-05 15:02:46 +0300

    fixes, auth for protected areas and token to firebase

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -17,15 +17,15 @@ export const purchaseBurgerFail = (error) => {
 };
 
 export const purchaseBurgerStart = () => {
-  return{
+  return {
     type: actionTypes.PURCHASE_BURGER_START
   };
 };
 
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData, token) => {
   return dispatch => {
     dispatch(purchaseBurgerStart());
-    axios.post('/orders.json', orderData)
+    axios.post('/orders.json?auth=' + token, orderData)
       .then(response => {
         console.log(response.data);
         dispatch(purchaseBurgerSuccess(response.data.name, orderData));
@@ -51,7 +51,7 @@ export const fetchOrdersSuccess = (orders) => {
 
 export const fetchOrdersFail = (error) => {
   return {
-    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    type: actionTypes.FETCH_ORDERS_FAIL,
     error: error
   };
 };
@@ -63,10 +63,10 @@ export const fetchOrdersStart = (orders) => {
 };
 
 
-export const fetchOrders = () => {
+export const fetchOrders = (token) => {
   return dispatch => {
     dispatch(fetchOrdersStart());    
-    axios.get('/orders.json')
+    axios.get('/orders.json?auth=' + token)
       .then(res => {
         const fetchedOrders = [];
         for (let key in res.data) {
